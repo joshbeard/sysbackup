9213889563b38fe0f1f0506bdf66fda438b74012
Josh Beard <josh@signalboxes.net>
2012-05-22 22:24:00 -0600

    Bump to v2012-05-22

--- sysbackup.sh
+++ sysbackup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #############################################################################
 # sysbackup
-# Last Modified: Wed 01 Feb 2012 05:58:28 PM MST by jbeard
+# Last Modified: Tue 22 May 2012 10:11:09 PM MDT by jbeard
 # 
 # Fairly simple backup script, using rsync
 # TODO:
+#	* Trackdown an issue where empty backups are created if connectivity is lost(?)
+#		* I think this happens if the remote data is unavailable or if we don't have
+#		* permission
+#	* Use logger for logging
+#	* Port to pure Bourne shell (sh)
 #	* Add sanity checks
 #	* Somehow check the real size of the backup before running
 #	* Add argument parsing
@@ -17,16 +22,16 @@ trap my_trap INT
 config_file=""
 
 # Address of the host to backup to/from
-bk_host="topeka"
+bk_host="192.168.1.120"
 
 # Remote user
 # If pulling, this user should have read rights to the data being pulled
 # If pushing, beware that ownership and ACLs won't be preserved (unless
 #   remote user is root [bad idea])
-bk_user="sysbackup"
+bk_user="remoteuser"
 
 # Path to backup to (local or remote)
-bk_path="/data/sysbackup"
+bk_path="/data/backups"
 
 # Path to SSH key to use
 ssh_key="/home/sysbackup/.ssh/id_rsa"
@@ -34,9 +39,14 @@ ssh_key="/home/sysbackup/.ssh/id_rsa"
 # Arguments to pass to SSH (also used in the rsync command)
 ssh_args="-F /home/sysbackup/.ssh/config -q -i $ssh_key"
 
+### Rsync Options ###
 # Don't pass the (-e) here, as it will be added below
-rsync_args="-azXA --progress --stats"
+# The following is an example of specifying the rsync path on the remote machine
+#rsync_args="-avzXA --progress --stats --rsync-path=\"/usr/bin/sudo /usr/bin/rsync\""
+rsync_args="-avXA --progress --stats"
+# The path to the rsync binary on this machine
 rsync_bin="/usr/bin/rsync"
+# The full path to an optional filter file
 rsync_filter="/etc/sysbackup/filter.txt"
 
 # 0 = pull from the remote host
@@ -50,24 +60,21 @@ check_if_up=1
 log_file="/var/log/sysbackup.log"
 log_date_fmt="+%F %T: "
 
+# E-mail report
+# Leave empty to disable
+mail_to="me@mydomain.tld"
+mail_only_errors=1
+
 # Verbose shows what commands are being executed
 verbose=1
 
 # Array of paths to backup
+# You can use "/" to backup the entire system
+# If you choose "/", the backup name will be $bk_host.root
 data_locs=( 
-"/media/store/homes/bb"
-"/media/store/homes/bc"
-"/media/store/homes/bg"
-"/media/store/homes/boe"
-"/media/store/homes/ca"
-"/media/store/homes/co"
-"/media/store/homes/fc"
-"/media/store/homes/fs"
-"/media/store/homes/hs"
-"/media/store/homes/imc"
-"/media/store/homes/ms"
-"/media/store/homes/td"
-"/media/store/homes/vn"
+"/svr/important_data"
+"/etc"
+"/home"
 )
 
 
@@ -91,7 +98,7 @@ calculate_free_space=1
 #backup_file_list=$(mktemp /tmp/backup.$(date $date_fmt).XXX || exit 1)
 #free_padding=200
 
-i_have_configured=1
+i_have_configured=0
 
 #############################################################################
 # END OF CONFIGURATION
@@ -101,11 +108,13 @@ i_have_configured=1
 if [ ! -z "$config_file" ]; then
 	if [ -e "$config_file" ]; then
 		if ! source "$config_file"; then
-			log_print "FAILURE: Unable to include $config_file\n"
+			err="FAILURE: Unable to include $config_file\n"
+			log_print "$err" ; send_email "$err"
 			quit 1
 		fi
 	else
-		log_print "FAILURE: config_file is set, but $config_file doesn't exist.\n"
+		err="FAILURE: config_file is set, but $config_file doesn't exist.\n"
+		log_print "$err" ; send_email "$err"
 		quit 1
 	fi
 fi
@@ -113,6 +122,32 @@ fi
 #############################################################################
 # Functions
 #############################################################################
+
+function send_email() {
+
+	okaytomail=0
+	# FIXME; This sucks.  Need the multiple if condition to work :\
+	[ "$mail_only_errors" -ne 1 ] && okaytomail=1
+	if [ "$mail_only_errors" -eq 1 ]; then
+		if [ ! -z "$3" ] && [ "$3" == "err" ]; then
+			okaytomail=1
+		fi
+	fi
+	#if [ "$mail_only_errors" -ne 1 ] || [ [ "$mail_only_errors" -eq 1 ] && [ ! -z "$3" ] && [ "$3" == "err" ] ]; then
+	if [ "$okaytomail" -eq 1 ]; then
+		[ ! -z "$2" ] && subject="$2" || subject=""
+		
+		if [ ! -z "$mail_to" ]; then
+			message="$1\n\n--\nsysbackup running on $(hostname)\n"
+			printf "$message" | mail -s "$(hostname) backup report ${subject}" $mail_to
+		fi
+	fi
+}
+
+# Add message to e-mail
+function add_email() {
+	email_msg="${email_msg}$(date '+%Y-%m-%d %H:%M') ${1}\n"
+}
 
 function human_readable() {
 	echo "$1"|awk '{ sum=$1 ; hum[1024**2]="GB";hum[1024**1]="MB";hum[1024]="KB"; for (x=1024**3; x>=1024; x/=1024){ if (sum>=x) { printf "%.2f %s\n",sum/x,hum[x];break } }}'
@@ -181,7 +216,7 @@ function log_print() {
 
 #############################################################################
 today="$(date $date_fmt)"
-ver="2012-01-31"
+ver="2012-05-22"
 #############################################################################
 
 
@@ -197,8 +232,9 @@ if [ -e "$pid_file" ]; then
 		log_print "$0 is already running (pid: ${my_pid})\n"
 		exit 1
 	else
-		log_print "${pid_file} reports a PID of ${my_pid}, but it doesn't seem to be running.\n"
-		log_print "If it's really not running, please remove ${pid_file}\n"
+		err="${pid_file} reports a PID of ${my_pid}, but it doesn't seem to be running.\n"
+		err="$err If it's really not running, please remove ${pid_file}\n"
+		log_print "$err" ; send_email "$err" "FAILURE" "err"
 		exit 1
 	fi
 fi
@@ -231,7 +267,8 @@ if [ $check_if_up -eq 1 ]; then
 	log_print "Checking if host $bk_host is up... "
 	if ! is_host_up $bk_host; then
 		log_print "failed\n"
-		log_print "FAILURE: $bk_host is unreachable (via ICMP)\n"
+		err="FAILURE: $bk_host is unreachable (via ICMP)\n"
+		log_print "$err" ; send_email "$err" "FAILURE" "err"
 		quit 1
 	else
 		log_print "ok\n"
@@ -249,7 +286,8 @@ if [ "$calculate_free_space" -eq 1 ]; then
 
 	# Need to determine if bk_path exists first
 	if ! verify_bkpath $bk_path; then
-		log_print "FAILURE: $bk_path doesn't exist ${where}, which we need to determine accurate free space.\n"
+		err="FAILURE: $bk_path doesn't exist ${where}, which we need to determine accurate free space.\n"
+		log_print "$err" ; send_email "$err" "FAILURE" "err"
 		quit 1
 	fi
 
@@ -259,11 +297,13 @@ if [ "$calculate_free_space" -eq 1 ]; then
 		free_space=$(ssh $ssh_args $bk_user@$bk_host "df -kP $bk_path|tail -1|awk '{print \$4}'")
 	fi
 
-	log_print " => Free space: $free_space KB ($(human_readable $free_space))\n"
-	log_print " => Minimum required free space: $min_free_space KB ($(human_readable $min_free_space))\n"	
+	msg=" => Free space: $free_space KB ($(human_readable $free_space))\n"
+	msg="$msg => Minimum required free space: $min_free_space KB ($(human_readable $min_free_space))\n"	
+	log_print "$msg" ; add_email "$msg"
 
 	if [ "$min_free_space" -ge "$free_space" ]; then
-		log_print " >> FAILURE: There is not enough free space ${where} to accomodate this backup\n"
+		err=" >> FAILURE: There is not enough free space ${where} to accomodate this backup\n"
+		log_print "$err" ; send_email "$err" "FAILURE" "err"
 		quit 1
 	fi
 
@@ -294,28 +334,60 @@ fi # calculate_free_space
 # The actual rsync process
 ##################################
 for data in "${data_locs[@]}"; do
-	relative=$(basename $data)
+
+	if ! is_host_up $bk_host; then
+		err="FAILURE: lost connectivity to $bk_host (via ICMP)\n"
+		log_print "$err" ; send_email "$err" "FAILURE" "err"
+		quit 1
+	fi
+
+	# This is sloppy.  If we're backing up root, name it after the host.root
+	if [ "$data" == "/" ]; then
+		relative="${bk_host}.root"
+	else
+		relative=$(basename $data)
+	fi
 
 	if [ $backup_method = 0 ]; then
 		if [ -e "$bk_path/$relative" ]; then
 			find $bk_path/$relative -maxdepth 1 -mtime +$max_age -exec echo 'Removing {}' \; -exec rm -rf {} \;
+			msg="Removing backups older than $max_age days\n"
+			log_print "$msg" ; add_email "$msg"
 		else
-			log_print "Creating ${bk_path}/${relative}\n"
+			msg="Creating ${bk_path}/${relative}\n"
+			log_print "$msg" ; add_email "$msg"
 			mkdir -p "$bk_path/$relative"
 		fi
 
-		rsync_string="${bk_user}@${bk_host}:${data}/. ${bk_path}/$relative/${today}.inprogress"
+		if [ $data == "/" ]; then
+			echo "here it is"
+			rsync_string="${bk_user}@${bk_host}:/"
+		else
+			rsync_string="${bk_user}@${bk_host}:${data}/"
+		fi
+
+		rsync_string="${rsync_string} ${bk_path}/$relative/${today}.inprogress"
+
 	else 
-		[ $verbose -eq 1 ] && log_print "Executing: ssh $ssh_args $bk_user@$bk_host \"find $bk_path/$relative -maxdepth 1 -mtime +$max_age -exec echo 'Removing {}' \; -exec rm -rf {} \;\"\n"
+		msg="Removing backups older than $max_age\n"
+		msg="${msg}Executing: ssh $ssh_args $bk_user@$bk_host \"find $bk_path/$relative -maxdepth 1 -mtime +$max_age -exec echo 'Removing {}' \; -exec rm -rf {} \;\"\n"
+		[ $verbose -eq 1 ] && log_print "$msg" ; add_email "$msg"
 		ssh $ssh_args $bk_user@$bk_host "[ -e \"$bk_path/$relative\" ] && find $bk_path/$relative -maxdepth 1 -mtime +$max_age -exec echo 'Removing {}' \; -exec rm -rf {} \; || mkdir -p \"$bk_path/$relative\""
-		rsync_string="${data}/. ${bk_user}@${bk_host}:${bk_path}/$relative/${today}.inprogress"
+		rsync_string="${data}/. ${bk_host}:${bk_path}/$relative/${today}.inprogress"
 	fi
 
-	[ $verbose -eq 1 ] && log_print "Executing: $rsync_bin $rsync_args -e \"ssh $ssh_args\" --link-dest=\"${bk_path}/$relative/Latest\" --stats ${rsync_string}\n"
-	$rsync_bin $rsync_args -e "ssh $ssh_args" --link-dest="${bk_path}/$relative/Latest" ${rsync_string}
+	msg="Backing up ${data}\n"
+	msg="${msg}Executing: $rsync_bin $rsync_args -e \"ssh $ssh_args -l ${bk_user}\" --link-dest=\"${bk_path}/$relative/Latest\" --stats ${rsync_string}\n"
+
+	[ $verbose -eq 1 ] && log_print "$msg" ; add_email "$msg"
+
+	rsync_cmd="$rsync_bin $rsync_args -e \"ssh $ssh_args\" --link-dest=\"${bk_path}/$relative/Latest\" ${rsync_string}"
+	eval $rsync_cmd
+	#$rsync_bin $rsync_args -e "ssh $ssh_args" --link-dest="${bk_path}/$relative/Latest" ${rsync_string}
 
 	if [ $backup_method = 0 ]; then
-			log_print "Moving ${today}.inprogress to ${today} and symlinking to Latest\n"
+			msg="Moving ${today}.inprogress to ${today} and symlinking to Latest\n"
+			log_print "$msg" ; add_email "$msg"
 			cd $bk_path/$relative
 			mv $today.inprogress $today
 			rm -f Latest;ln -s $today Latest
@@ -323,14 +395,21 @@ for data in "${data_locs[@]}"; do
 			# Update the time so the find routine will work right
 			touch $today
 	else
-		[ $verbose -eq 1 ] && log_print "Executing: ssh $ssh_args $bk_user@$bk_host \"mv $bk_path/$today.inprogress $bk_path/$relative/$today;cd $bk_path/$relative;rm -f Latest;ln -s $today Latest;touch $today\"\n"
+		msg="Moving $today}.inprogress to ${today} and symlinking to Latest\n"
+		msg="${msg}Executing: ssh $ssh_args $bk_user@$bk_host \"mv $bk_path/$today.inprogress $bk_path/$relative/$today;cd $bk_path/$relative;rm -f Latest;ln -s $today Latest;touch $today\"\n"
+
+		[ $verbose -eq 1 ] && log_print "$msg" ; add_email "$msg"
 		ssh $ssh_args $bk_user@$bk_host "mv $bk_path/$relative/$today.inprogress $bk_path/$relative/$today;cd $bk_path/$relative;rm -f Latest;ln -s $today Latest;touch $today"
 	fi
 
 done
 
-    
-log_print "==> Completed\n" 
+msg="==> Completed\n" 
+log_print "$msg"
+add_email "$msg"
+
+send_email "$email_msg"
+
 
 quit 0
 #EOF
